0331752160e4e1cb5a9ba06e7b7cbea1e6d89205
IrynaS1 <snowqueen-2007@mail.ru>
2023-11-10 14:14:51 +0300

    ready spetsifikatsia page

--- personal/spetsifikatsia/script.js
+++ personal/spetsifikatsia/script.js
@@ -42,7 +42,67 @@ mobCitySelect.addEventListener('change', function () {
 	mobTooltip.classList.add('footer-select-city');
 });
 
-const viewRow = document.querySelector('.view-row'),
+const regBtn = document.querySelector('.header-menu__reg'),
+	overlay = document.querySelector('.overlay'),
+	modalReg = document.querySelector('.modal-reg'),
+	modalAuth = document.querySelector('.modal-auth'),
+	modalRecovery = document.querySelector('.modal-recovery'),
+	inputsPassword = document.querySelectorAll('.modal-form__input-password'),
+	authBtn = document.querySelector('.header-menu__auth');
+
+regBtn.addEventListener('click', function () {
+	overlay.classList.add('overlay--open');
+	modalReg.classList.add('modal--open');
+});
+
+authBtn.addEventListener('click', function () {
+	overlay.classList.add('overlay--open');
+	modalAuth.classList.add('modal--open');
+});
+
+const recoveryBtn = document.querySelector('.recovery-password');
+recoveryBtn.addEventListener('click', function () {
+	modalAuth.classList.remove('modal--open');
+	modalRecovery.classList.add('modal--open');
+});
+
+const registrationBtn = document.querySelector('.registration');
+registrationBtn.addEventListener('click', function () {
+	modalAuth.classList.remove('modal--open');
+	modalReg.classList.add('modal--open');
+});
+
+inputsPassword.forEach(function (el) {
+	el.addEventListener('click', function (e) {
+		const passwordEye = e.target.nextElementSibling;
+		passwordEye.classList.add('show--password');
+		passwordEye.addEventListener('click', function () {
+			passwordEye.classList.toggle('show--password');
+			passwordEye.classList.toggle('close--password');
+			if (el.getAttribute('type') === 'password') {
+				el.setAttribute('type', 'text');
+			} else {
+				el.setAttribute('type', 'password');
+			}
+		});
+	});
+});
+
+overlay.addEventListener('click', function () {
+	overlay.classList.remove('overlay--open');
+	modalReg.classList.remove('modal--open');
+	modalAuth.classList.remove('modal--open');
+	modalRecovery.classList.remove('modal--open');
+});
+
+const footerLocationBtn = document.querySelector('.footer__mob-location');
+footerLocationBtn.addEventListener('click', function () {
+	const mobLocation = document.querySelector('.footer-block__location-city-select');
+	mobLocation.classList.toggle('footer-select-city');
+});
+
+const/*  centralBlock = document.querySelector('.central-block'), */
+	viewRow = document.querySelector('.view-row'),
 	viewCell = document.querySelector('.view-cell'),
 	goodsBlockRow = document.querySelector('.goods-row'),
 	goodsBlockCell = document.querySelector('.goods-cell'),
@@ -73,38 +133,34 @@ viewRow.addEventListener('click', function () {
 const viewCellCard = document.querySelector('.good-cell-item'),
 	viewCellCardFooter = document.querySelector('.good-cell-card__footer');
 
-viewCellCard.addEventListener('mousemove', function () {
-	viewCellCardFooter.style.display = 'block';
+function deleteViewRow() {
+	const widthBody = window.innerWidth;
+	if (widthBody <= 1430) {
+		const optionsBlock = document.querySelector('.goods__view-block');
+		optionsBlock.style.display = 'none';
+		goodsBlockRow.classList.remove('view--active');
+		goodsBlockCell.classList.add('view--active');
+		viewCellCardFooter.style.display = 'block';
+	}/* Поправить раскрытие карточки по наведению мыши
+	 else {                   
+		viewCellCard.addEventListener('mousemove', function () {
+			viewCellCardFooter.style.display = 'block';
+		});
+
+		viewCellCard.addEventListener('mouseleave', function () {
+			viewCellCardFooter.style.display = 'none';
+		});
+	} */
+}
+
+window.addEventListener('resize', function () {
+	deleteViewRow();
 });
 
-viewCellCard.addEventListener('mouseleave', function () {
-	viewCellCardFooter.style.display = 'none';
-});
-
-//МОДАЛЬНОЕ ОКНО РЕГИСТРАЦИИ
-const regBtn = document.querySelector('.header-menu__reg'),
-	overlay = document.querySelector('.overlay'),
-	modalReg = document.querySelector('.modal-reg'),
-	modalAuth = document.querySelector('.modal-auth'),
-	authBtn = document.querySelector('.header-menu__auth'),
-	mobAuth = document.querySelector('.footer__mob-item-btn_auth');
-
-regBtn.addEventListener('click', function () {
-	overlay.classList.add('overlay--open');
-	modalReg.classList.add('modal--open');
-});
-
-authBtn.addEventListener('click', function () {
-	overlay.classList.add('overlay--open');
-	modalAuth.classList.add('modal--open');
-});
-
-mobAuth.addEventListener('click', function () {
-	overlay.classList.add('overlay--open');
-	modalAuth.classList.add('modal--open');
-})
-
-overlay.addEventListener('click', function () {
-	overlay.classList.remove('overlay--open');
-	modalReg.classList.remove('modal--open');
+const goodsCards = document.querySelectorAll('.good-cell-item');
+goodsCards.forEach(function (el) {
+	el.addEventListener('click', function () {
+		el.classList.add('card--open');
+		viewCellCardFooter.style.display = 'block';
+	})
 });
